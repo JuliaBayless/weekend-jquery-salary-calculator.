67ca007ff08afd7e7a2b9b8014e5914c760151fa
Julia Bayless <julia.allistair@gmail.com>
2021-10-01 15:48:00 -0500

    employee information appending to DOM

--- client.js
+++ client.js
@@ -9,15 +9,24 @@ function readyNow() {
 
 function addEmployeeInformation() {
     console.log('in addEmployeeInformation');
+    
 
     const employeeIn = {
         firstName: $(`#firstNameIn`).val(),
         lastName: $(`#lastNameIn`).val(),
         id: $(`#idNumberIn`).val(),
         Title: $(`#jobTitleIn`).val(),
-        Salary: $(`salaryIn`).val()
+        Salary: $(`#salaryIn`).val()
     }
     console.log(employeeIn);
 
+    const row = `<tr>
+    <td>${employeeIn.firstName}</td>
+    <td>${employeeIn.lastName}</td>
+    <td>${employeeIn.id}</td>
+    <td>${employeeIn.Title}</td>
+    <td>${employeeIn.Salary}</td>
+    </tr>`
 
+    $(`#informationList`).append(row);
 }
