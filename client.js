2ce5a362235ec74374b975fefffa98d2defd889f
Julia Bayless <julia.allistair@gmail.com>
2021-10-01 15:40:05 -0500

    functionally logging employee information

--- client.js
+++ client.js
@@ -1,16 +1,23 @@
 $(readyNow);
 
-function readyNow(){
+function readyNow() {
     console.log('JQ is Loaded!');
-    
+
     $(`#submitButton`).on(`click`, addEmployeeInformation)
 
 } //end readyNow
 
-function addEmployeeInformation(){
+function addEmployeeInformation() {
     console.log('in addEmployeeInformation');
-    
+
     const employeeIn = {
-        
+        firstName: $(`#firstNameIn`).val(),
+        lastName: $(`#lastNameIn`).val(),
+        id: $(`#idNumberIn`).val(),
+        Title: $(`#jobTitleIn`).val(),
+        Salary: $(`salaryIn`).val()
     }
+    console.log(employeeIn);
+
+
 }
